796e4d1c01542d84fce10bee0c6f7f1a925f6d78
Alex Lvov <grssnake@gmail.com>
2019-04-02 07:53:47 +0300

    eyes

---
+++ face/pygame-example.py
@@ -0,0 +1,120 @@
+#! /usr/bin/python3
+
+import os
+import pygame
+pygame.init()
+
+SIZE = WIDTH, HEIGHT = 480, 800
+BACKGROUND_COLOR = pygame.Color('black')
+FPS = 60
+
+screen = pygame.display.set_mode(SIZE)
+clock = pygame.time.Clock()
+
+
+def load_images(path):
+    """
+    Loads all images in directory. The directory must only contain images.
+    Args:
+        path: The relative or absolute path to the directory to load images from.
+    Returns:
+        List of images.
+    """
+    images = []
+    for file_name in os.listdir(path):
+        image = pygame.image.load(path + os.sep + file_name).convert()
+        images.append(image)
+    return images
+
+
+class AnimatedSprite(pygame.sprite.Sprite):
+
+    def __init__(self, position, images):
+        """
+        Animated sprite object.
+
+        Args:
+            position: x, y coordinate on the screen to place the AnimatedSprite.
+            images: Images to use in the animation.
+        """
+        super(AnimatedSprite, self).__init__()
+
+        size = (450, 450)  # This should match the size of the images.
+
+        self.rect = pygame.Rect(position, size)
+        self.images = images
+        self.images_right = images
+        self.images_left = [pygame.transform.flip(image, True, False) for image in images]  # Flipping every image.
+        self.index = 0
+        self.image = images[self.index]  # 'image' is the current image of the animation.
+
+        self.velocity = pygame.math.Vector2(0, 0)
+
+        self.animation_time = 0.5
+        self.current_time = 0
+
+        self.animation_frames = 11
+        self.current_frame = 0
+
+    def update_time_dependent(self, dt):
+        """
+        Updates the image of Sprite approximately every 0.1 second.
+
+        Args:
+            dt: Time elapsed between each frame.
+        """
+        if self.velocity.x > 0:  # Use the right images if sprite is moving right.
+            self.images = self.images_right
+        elif self.velocity.x < 0:
+            self.images = self.images_left
+
+        self.current_time += dt
+        if self.current_time >= self.animation_time:
+            self.current_time = 0
+            self.index = (self.index + 1) % len(self.images)
+            self.image = self.images[self.index]
+
+        self.rect.move_ip(*self.velocity)
+
+    def update_frame_dependent(self):
+        """
+        Updates the image of Sprite every 6 frame (approximately every 0.1 second if frame rate is 60).
+        """
+        if self.velocity.x > 0:  # Use the right images if sprite is moving right.
+            self.images = self.images_right
+        elif self.velocity.x < 0:
+            self.images = self.images_left
+
+        self.current_frame += 1
+        if self.current_frame >= self.animation_frames:
+            self.current_frame = 0
+            self.index = (self.index + 1) % len(self.images)
+            self.image = self.images[self.index]
+
+        self.rect.move_ip(*self.velocity)
+
+    def update(self, dt):
+        """This is the method that's being called when 'all_sprites.update(dt)' is called."""
+        # Switch between the two update methods by commenting/uncommenting.
+        self.update_time_dependent(dt)
+        # self.update_frame_dependent()
+
+
+def main():
+    images = load_images(path='eyes')
+    player = AnimatedSprite(position=(15, 100), images=images)
+    all_sprites = pygame.sprite.Group(player)  # Creates a sprite group and adds 'player' to it.
+
+    running = True
+    while running:
+
+        dt = clock.tick(FPS) / 1000  # Amount of seconds between each loop.
+        all_sprites.update(dt)  # Calls the 'update' method on all sprites in the list (currently just the player).
+
+        screen.fill(BACKGROUND_COLOR)
+        all_sprites.draw(screen)
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    main()
