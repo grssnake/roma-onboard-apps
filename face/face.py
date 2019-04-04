bac09be6cf9ae390b0b7f46364895e9a16272d8e
Alex Lvov <lvovaj@mgpu.ru>
2019-04-04 06:17:58 +0300

    create simple animation

---
+++ face/face.py
@@ -0,0 +1,68 @@
+#! /usr/bin/python3
+
+import os, sys
+import pygame
+from time import sleep
+
+pygame.init()
+
+SIZE = WIDTH, HEIGHT = 480, 800
+BACKGROUND_COLOR = pygame.Color('black')
+FPS = 60
+
+screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption('Roma Face')
+clock = pygame.time.Clock()
+
+def load_images(path):
+    """
+    Loads all images in directory. The directory must only contain images.
+    Args:
+        path: The relative or absolute path to the directory to load images from.
+    Returns:
+        List of images.
+    """
+    images = []
+    for file_name in os.listdir(path):
+        image = pygame.image.load(path + os.sep + file_name).convert()
+        images.append(image)
+    return images
+
+def main():
+    images = load_images(path='eyes')
+    images_left = [pygame.transform.flip(image, True, False) for image in images]  # Flipping every image.
+    sprites = [pygame.sprite.Sprite()]
+
+    seq = [
+        [images[7], 5],
+        [images[4], 0.5],
+        [images_left[4], 0.5],
+        [images[7], 5],
+        [images[4], 0.5],
+        [images_left[4], 0.5],
+        [images[3], 1],
+    ]
+
+    r1 = images[4].get_rect()
+    r2 = images_left[4].get_rect()
+    r3 = images[7].get_rect()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+
+        dt = clock.tick(FPS) / 1000  # Amount of seconds between each loop.
+
+        for i in seq:
+            screen.fill(BACKGROUND_COLOR)
+            screen.blit(i[0], i[0].get_rect())
+            pygame.display.update()
+            sleep(i[1])
+
+#        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    main()
+
