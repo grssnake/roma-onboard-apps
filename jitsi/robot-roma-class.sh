7d1680f15be57de61d4bbdfc2cb382367b10eb66
Alex Lvov <lvovaj@mgpu.ru>
2019-04-15 10:38:56 +0300

    adopt for remote execute add start/stop options

--- jitsi/robot-roma-class.sh
+++ jitsi/robot-roma-class.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
 
-export DISPLAY=:0
-chromium-browser --kiosk --disable-session-crashed-bubble --disable-infobars --disable-restore-session-state https://meet.jit.si/RobotRomaClass
+cmd_start="/tmp/j.start"
+cmd_stop="/tmp/j.stop"
+
+if [ -f "$cmd_start" ]
+then
+    export DISPLAY=:0
+	echo "$cmd_start found. Start jitsu"
+    rm -rf "$cmd_start"
+    chromium-browser --kiosk --disable-session-crashed-bubble --disable-infobars --disable-restore-session-state https://meet.jit.si/RobotRomaClass
+fi
+
+if [ -f "$cmd_stop" ]
+then
+	echo "$cmd_stop found. Stop jitsu"
+    rm -rf "$cmd_stop"
+    pkill -f -9 chromium
+else
+	echo "Nothing."
+fi
+
+
+
