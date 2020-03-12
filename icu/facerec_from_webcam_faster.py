c546399a319a1c5111ff99d78ab635225e42dcd8
Alex Lvov <grssnake@gmail.com>
2020-03-12 07:33:52 +0300

    test face id

--- icu/facerec_from_webcam_faster.py
+++ icu/facerec_from_webcam_faster.py
@@ -19,8 +19,8 @@ alex_image = face_recognition.load_image_file("/home/pi/Pictures/known_faces/Ale
 alex_face_encoding = face_recognition.face_encodings(alex_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-sergey_image = face_recognition.load_image_file("/home/pi/Pictures/known_faces/Sergey.jpg")
-sergey_face_encoding = face_recognition.face_encodings(sergey_image)[0]
+# sergey_image = face_recognition.load_image_file("/home/pi/Pictures/known_faces/Sergey.jpg")
+# sergey_face_encoding = face_recognition.face_encodings(sergey_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
