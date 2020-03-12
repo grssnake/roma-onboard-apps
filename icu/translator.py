d58f8708b95849e91a1177ac6f19881d38d8f2f4
Alex Lvov <grssnake@gmail.com>
2020-03-12 17:16:45 +0300

    test face id

--- icu/translator.py
+++ icu/translator.py
@@ -1,3 +1,5 @@
+from espeakng import ESpeakNG
+
 
 words = {
     "carrot": "морковь",
@@ -7,11 +9,21 @@ words = {
     "fish": "рыба",
     "bread": "хлеб",
     "tumbler": "неваляшка",
-    "": "",
-    "": "",
-    "": "",
-    "": "",
-    "": "",
-    "": "",
-    "": "",
+    "car": "машина",
+    "tree": "дерево",
+    "ship": "корабль",
+    "airplane": "самолет",
+    "satellite": "спутник",
+    "moon": "луна",
+    "sun": "солнце",
+    "cosmonaut": "космонавт",
 }
+
+engine = ESpeakNG()
+engine.voice = 'english'
+engine.speed = 100
+engine.pitch = 32
+engine.voice = 'russian'
+
+engine.say('Привет! Меня зовут Рома. Я очень рад вас видеть!', sync=True)
+
